a3f0edd3acf4046bdb84e459b7cc48dce481ab83
hosead6168 <dominique11h@yahoo.com>
2021-11-12 12:07:16 -0600

    feat(footer): added footer

---
+++ client/src/components/footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <>
+            <footer class="container">
+                <p class="float-end"><a href="#top">Back to top</a></p>
+                <p>&copy; 2020–2021 Hoseacodes &middot; <a href="/privacy">Privacy</a> &middot; <a href="/terms">Terms</a></p>
+            </footer>
+        </>
+    )
+}
+
+export default Footer;
