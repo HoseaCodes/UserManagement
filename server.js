5daf3d779cb5ffed2eca8ebd38b53656da16f2db
hosead6168 <dominique11h@yahoo.com>
2021-11-12 11:57:00 -0600

    mfeat(server): added backend server

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const colors = require('colors');
+const morgan = require('morgan');
+const connectDB = require("./config/database");
+const path = require('path');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const fileUpload = require("express-fileupload");
+
+dotenv.config({ path: './config/config.env' })
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(
+	fileUpload({
+		useTempFiles: true,
+	})
+);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+//Mounted Routes
+const userRouter = require('./routes/user');
+const uploadRouter = require('./routes/upload');
+
+app.use('/user', userRouter);
+app.use("/api", uploadRouter);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
