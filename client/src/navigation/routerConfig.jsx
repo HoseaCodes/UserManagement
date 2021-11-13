e2ec6bd22d69c83e494c898be2c074e0df0972f8
hosead6168 <dominique11h@yahoo.com>
2021-11-12 18:29:15 -0600

    fix(app): removed unused pages

--- client/src/navigation/routerConfig.jsx
+++ client/src/navigation/routerConfig.jsx
@@ -6,11 +6,11 @@ import { NotFound } from "./notfound";
 import { ROOT, DETAILS, LOGIN, SIGNUP, PROFILE, CREATE, EDIT } from "./constants";
 import Login from "./auth/login";
 import Signup from "./auth/register";
-import Profile from "../pages/profile/profile";
+// import Profile from "../pages/profile/profile";
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
 import CreateUser from "../pages/createUser";
-import EditUser from "../pages/editUser";
+// import EditUser from "../pages/editUser";
 
 export const RouterConfig = () => {
   return (
@@ -23,8 +23,8 @@ export const RouterConfig = () => {
           <Route exact path={LOGIN} component={Login} />
           <Route exact path={SIGNUP} component={Signup} />
           <Route exact path={CREATE} component={CreateUser} />
-          <Route exact path={EDIT} component={EditUser} />
-          <Route exact path={PROFILE} component={Profile} />
+          {/* <Route exact path={EDIT} component={EditUser} /> */}
+          {/* <Route exact path={PROFILE} component={Profile} /> */}
           {/* List a generic 404-Not Found route here */}
           <Route path="*" component={NotFound}/>
         </Switch>
