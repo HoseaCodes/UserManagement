4df7ab9412abeec6fe9bd1edad89dcb08b61739a
hosead6168 <dominique11h@yahoo.com>
2021-11-12 19:02:42 -0600

    fix(client): updated display

--- client/src/components/display.jsx
+++ client/src/components/display.jsx
@@ -11,16 +11,15 @@ const Display = () => {
                 <hr class="featurette-divider"/>
                 <div class="row display-row">
                     <User/> 
-                <hr class="featurette-divider"/>
-                </div>
-                <h2 className="text-center">Unregistered *Demo* Users</h2>
-                <hr class="featurette-divider"/>
-                <div class="row display-row">
-                    <UnregisteredUsers/>
                 </div>
                 <hr class="featurette-divider"/>
                 <div class="row ">
                     <p className="display-row"><a class="btn btn-secondary" href="/create">Add Demo Profile &raquo;</a></p>
+                </div>
+                <hr class="featurette-divider"/>
+                <h2 className="text-center">Unregistered *Demo* Users</h2>
+                <div class="row display-row">
+                    <UnregisteredUsers/>
                 </div>
                 <hr class="featurette-divider"/>
             </div>
