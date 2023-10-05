184c468a20d21f80e270da5c6a332b77b1edb9eb
Dominique Hosea <dominique11h@yahoo.com>
2023-10-05 03:58:47 -0500

    fix(*): update backend upload route

--- routes/upload.js
+++ routes/upload.js
@@ -10,7 +10,7 @@ cloudinary.config({
 
 //image upload
 router.post(
-	"/upload",(req, res) => {
+	"/upload", async (req, res) => {
 		try {
 			if (!req.files || Object.keys(req.files).length === 0)
 				return res.status(400).send({ msg: "No files were uploaded." });
@@ -21,22 +21,27 @@ router.post(
 				return res.status(400).json({ msg: "File size too large" });
 			}
 
-			if (file.mimetype !== "image/jpeg" && file.mimetype !== "image/png") {
+			if (file.mimetype !== "image/jpeg" || file.mimetype !== "image/jpg" || file.mimetype !== "image/png") {
+				console.log('here')
+			} else {
 				removeTmp(file.tempFilePath);
 				return res.status(400).json({ msg: "File format is incorrect" });
-			}
+			}			
 
-			cloudinary.v2.uploader.upload(
-				file.tempFilePath,
-				{ folder: "PolticallySavvy" },
-				async (err, result) => {
-					if (err) throw err;
+			const cloudinaryUpload = async () => {
+				try {
+					const data = await cloudinary.v2.uploader.upload(file.tempFilePath, {
+					 folder: "UserManagement" 
+					});
+					return data;
+				} catch (e) {
+					console.log(e.message, e);
+					return {result: ""};
 
-					removeTmp(file.tempFilePath);
-
-					return res.json({ result });
 				}
-			);
+			};
+			const result = await cloudinaryUpload()
+			return res.json({result});
 		} catch (err) {
 			console.log(err)
 			return res.status(500).json({ msg: err.message });
