58aca02615aec9443c48fb95d4d2968d88a1c4af
Dominique Hosea <dominique11h@yahoo.com>
2023-10-07 01:12:42 -0500

    fix(*): update notfound

--- client/src/navigation/notfound.jsx
+++ client/src/navigation/notfound.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {Link} from 'react-router-dom'
 
-export const NotFound = () => {
+export const NotFound2 = () => {
   return (
     <>
       {/* Page Not Found! */}
