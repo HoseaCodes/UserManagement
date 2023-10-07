b19eb844a7ec0c9170b19a14040ac97d97391c98
Dominique Hosea <dominique11h@yahoo.com>
2023-10-07 01:12:59 -0500

    fix(*): update routes

--- client/src/navigation/routerConfig.jsx
+++ client/src/navigation/routerConfig.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from "../pages/home";
 import Details from "../pages/createUser";
-import { NotFound } from "./notfound";
-import { ROOT, DETAILS, LOGIN, SIGNUP, PROFILE, CREATE, EDIT } from "./constants";
+import NotFound2 from "./notFound2";
+import { ROOT, DETAILS, LOGIN, SIGNUP, PROFILE, 
+  CREATE, EDIT, ALL, ADD, EDIT2 } from "./constants";
 import Login from "./auth/login";
 import Signup from "./auth/register";
 import Profile from "../pages/profile/profile";
@@ -11,6 +12,9 @@ import Navbar from "../components/navbar";
 import Footer from "../components/footer";
 import CreateUser from "../pages/createUser";
 import EditUser from "../pages/editUser";
+import AllUsers from "../pages/crud/allUsers";
+import AddUser from "../pages/crud/addUser";
+import EditAUser from "../pages/crud/editaUser";
 
 export const RouterConfig = () => {
   return (
@@ -26,8 +30,11 @@ export const RouterConfig = () => {
           <Route exact path={CREATE} render={() => <CreateUser/>} />
           <Route exact path={EDIT} render={() => <EditUser/>} />
           <Route exact path={PROFILE} render={() => <Profile/>} />
+          <Route exact path={ALL} render={() => <AllUsers /> } />
+          <Route exact path={ADD} render={() => <AddUser />} />
+          <Route exact path={EDIT2} render={() => <EditAUser />} />
           {/* List a generic 404-Not Found route here */}
-          <Route path="*" render={() => <NotFound/>}/>
+          <Route path="*" render={() => <NotFound2/>}/>
         </Switch>
       <Footer/>
       </div>
